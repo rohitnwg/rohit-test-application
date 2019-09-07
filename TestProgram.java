6eb22a0c7b8c82507a534808c439e9c67961f69f
rohitnwg <rohitnwg@g>
2019-09-07 23:08:00 +0530

    did changes for testing

--- TestProgram.java
+++ TestProgram.java
@@ -5,7 +5,7 @@ public class TestProgram {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Welcome in Git Hub Program.");
+		System.ou.println("Welcome in Git Hub Program.");
 
 	}
 
